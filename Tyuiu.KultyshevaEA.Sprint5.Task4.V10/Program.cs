f1be221578aa8b24b20b6168ebbe23547a84fff8
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-11-22 22:20:26 +0500

    Task4

---
+++ Tyuiu.KultyshevaEA.Sprint5.Task4.V10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.KultyshevaEA.Sprint5.Task4.V10.Lib;
+
+namespace Tyuiu.KultyshevaEA.Sprint5.Task4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнила: Култышева Е. А. | ИИПб-23-3";
+
+            
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнила: Култышева Екатерина Александровна | ИИПб-23-3                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Создать папку вручную С:DataSprint5 и скопировать в неё файл, в котором *");
+            Console.WriteLine("* есть вещественное значение. Прочитать значение из файла и подставить    *");
+            Console.WriteLine("* вместо Х в формуле y = x^3 * 1.2*x + 2                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask4V10.txt";
+
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
