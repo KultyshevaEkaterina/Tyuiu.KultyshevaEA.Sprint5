7b13a495e76eab961a45bd1b0960957a985eb1b7
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-11-23 20:01:04 +0500

    Task5

---
+++ Tyuiu.KultyshevaEA.Sprint5.Task5.V23.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KultyshevaEA.Sprint5.Task5.V23.Lib
+{
+    public class DataService : ISprint5Task5V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double min = 100000;
+            
+            using(StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                     if (Convert.ToDouble(line) < min)
+                     {
+                         min = Convert.ToDouble(line);
+                     }
+                    
+                }
+            }
+            return Math.Round(min,3);
+        }
+    }
+}
---
+++ Tyuiu.KultyshevaEA.Sprint5.Task5.V23/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.KultyshevaEA.Sprint5.Task5.V23.Lib;
+
+namespace Tyuiu.KultyshevaEA.Sprint5.Task5.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнила: Култышева Е. А. | ИИПб-23-3";
+
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Култышева Екатерина Александровна | ИИПб-23-3                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Создать папку вручную С:DataSprint5 и скопировать в неё файл, в котором *");
+            Console.WriteLine("* есть набор вещественных значений.                                       *");
+            Console.WriteLine("* Найти минимальное вещественное число в файле.                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask5V23.txt";
+
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Минимальное вещественное число в файле: " + res);
+            Console.ReadKey();
+        }
+    }
+}
